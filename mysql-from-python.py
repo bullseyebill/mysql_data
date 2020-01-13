cb3273cf44c9256e7036a55ea71b45efbf72087f
Rupert <rupertjhewson@gmail.com>
2020-01-13 17:13:03 +0000

    files

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # Get the username from the Cloud9 workspace
@@ -12,10 +13,11 @@ connection = pymysql.connect(host='localhost',
                              db='Chinook')
 
 try:
-    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        for row in cursor:
-            print(row)
+    with connection.cursor() as cursor:
+        rows = [("bob", 21, "1990-02-06 23:04:56"),
+                ("jim", 56, "1955-05-09 13:12:45"),
+                ("fred", 100, "1911-09-12 01:01:01")]
+        cursor.executemany("INSERT INTO Friends VALUES (%s,%s,%");", rows)
+        connection.commit()        
 finally:
     connection.close()
